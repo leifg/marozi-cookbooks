e454febd34e23057dd03301d514750fc6dfbce99
Leif Gensert <leif.gensert@stylefruits.de>
2013-01-22 17:40:23 +0100

    add metadata files

---
+++ ruby/README.md
@@ -0,0 +1,10 @@
+# Description
+
+relies on:
+  - [ruby-build](https://github.com/arkency/ruby-build-cookbook)
+
+## Recipes
+
+### default
+
+Install correct ruby version in home (inspired by [this article](http://blog.arkency.com/2012/11/one-app-one-user-one-ruby/)
--- ruby/metadata.rb
+++ ruby/metadata.rb
@@ -3,6 +3,7 @@ maintainer        'Leif Gensert'
 maintainer_email  'leif@gensert.de'
 license           'Apache 2.0'
 description       'manages the installation for the needed ruby version'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.0.1'
 
 depends 'ruby-build'
